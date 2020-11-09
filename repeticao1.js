327dba46c168645be121cb6c01acce55db3af5b3
Stefany-Souza <70451854+Stefany-Souza@users.noreply.github.com>
2020-11-09 13:20:06 -0300

    Add files via upload

---
+++ repeticao1.js
@@ -0,0 +1,8 @@
+let num1 = parseInt(prompt("Digite seu primeiro número"));
+let num2 = parseInt(prompt("Digite seu segundo número"));
+let num3 = parseInt(prompt("Digite seu terceiro número"));
+let num4 = parseInt(prompt("Digite seu quarto número"));
+const soma = num1 + num2 + num3 + num4;
+const media = soma / 4;
+
+alert(`A soma de seus números é ${soma} e a média é ${media}`);
